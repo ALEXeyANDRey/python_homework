936910e4b137bde157b0334c98c858288fdd0d08
Alex <Alex-f1n4@hotmail.com>
2022-04-23 14:37:00 +0300

    dobavil

---
+++ work7.py
@@ -0,0 +1,7 @@
+# проверить истинность утверждения
+for x in range(2):
+    for y in range(2):
+        for z in range(2):
+            if (not(x or y or z) == (not x and not y and not z)):
+                print(f'При x = {x}, y = {y}, z = {z} - истинно')
+            else: print (f'При x = {x}, y = {y}, z = {z} - ложно')    
