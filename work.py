8f6ee607980b873b360c5fa3a534ece3993f0665
Alex <Alex-f1n4@hotmail.com>
2022-05-02 20:14:50 +0300

    11-15

--- work.py
+++ work.py
@@ -1,1 +1,13 @@
 print('hello world')
+# import complex math module 
+import cmath 
+a = float(input('Enter a: ')) 
+b = float(input('Enter b: ')) 
+c = float(input('Enter c: ')) 
+ 
+
+ 
+# find two solutions 
+sol1 =(-b-cmath.sqrt(d))/(2*a) 
+sol2 =(-b+cmath.sqrt(d))/(2*a) 
+print('The solution are {0} and {1}'.format(sol1,sol2))  
